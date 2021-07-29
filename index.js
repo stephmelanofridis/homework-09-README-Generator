78aca866934cd5cef7bb3b8487b8b8078bf790fe
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-07-29 13:42:09 +1000

    Adding functions to display badges and licensing details

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fs = require("fs");
 const generateREADME = (answers) =>
     `
     # ${answers.title}
+    ${generateBadge(answers.license)}
 
     ${answers.description}
 
@@ -28,8 +29,7 @@ const generateREADME = (answers) =>
 
     ## License
 
-    
-    ${answers.license}
+    ${generateDescription(answers.license)}
 
     ## Contributions
 
@@ -50,7 +50,116 @@ const generateREADME = (answers) =>
 - [Back to top] (#Table-of-Contents) 
     `;
 
+const generateBadge = (license) => {
+    if (license.value == "Apache License 2.0") {
+        return license + " [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+    } else if (license.value == "MIT License") {
+        return license + " [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    } else if (license.value == "GNU General Public License v2.0") {
+        return license + " [![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)"
+    } else if (license.value == "Mozilla Public License") {
+        return license + " [![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+    } else if (license.value == "The Unilicense") {
+        return license + " [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+    } else {
+        return " "
+    }
+};
+
+const generateDescription = (license) => {
+    if (license.value == "Apache License 2.0") {
+        return license + `\n\nCopyright 2021 ${answers.name}
+            
+                Licensed under the Apache License, Version 2.0 (the "License");
+                you may not use this file except in compliance with the License.
+                You may obtain a copy of the License at
+                
+                    http://www.apache.org/licenses/LICENSE-2.0
+                
+                Unless required by applicable law or agreed to in writing, software
+                distributed under the License is distributed on an "AS IS" BASIS,
+                WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+                See the License for the specific language governing permissions and
+                limitations under the License.`
+
+    } else if (license.value == "MIT License") {
+        return license + `\n\nCopyright © 2021 ${answers.name}
+                
+                Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+                
+                The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+                
+                THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
+    } else if (license.value == "GNU General Public License v2.0") {
+        return license + `\n\nCopyright © 2021 ${answers.name}
+
+                This library is free software; you can redistribute it and/or
+                modify it under the terms of the GNU Library General Public
+                License as published by the Free Software Foundation; either
+                version 2 of the License, or (at your option) any later version.
+                
+                This library is distributed in the hope that it will be useful,
+                but WITHOUT ANY WARRANTY; without even the implied warranty of
+                MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+                Library General Public License for more details.
+                
+                You should have received a copy of the GNU Library General Public
+                License along with this library; if not, write to the
+                Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+                Boston, MA  02110-1301, USA.`
+
+    } else if (license.value == "Mozilla Public License 2.0") {
+        return license + `\n\nCopyright © 2021 ${answers.name}
+                
+                This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+                If it is not possible or desirable to put the notice in a particular file, then You may include the notice in a location (such as a LICENSE file in a relevant directory) where a recipient would be likely to look for such a notice.
+                
+                You may add additional accurate notices of copyright ownership.`
+    } else if (license.value == "The Unilicense") {
+        return license + `\n\nCopyright © 2021 ${answers.name}
+                This is free and unencumbered software released into the public domain.
+
+                Anyone is free to copy, modify, publish, use, compile, sell, or
+                distribute this software, either in source code form or as a compiled
+                binary, for any purpose, commercial or non-commercial, and by any
+                means.
+                
+                In jurisdictions that recognize copyright laws, the author or authors
+                of this software dedicate any and all copyright interest in the
+                software to the public domain. We make this dedication for the benefit
+                of the public at large and to the detriment of our heirs and
+                successors. We intend this dedication to be an overt act of
+                relinquishment in perpetuity of all present and future rights to this
+                software under copyright law.
+                
+                THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+                EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+                MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+                IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+                OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+                ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+                OTHER DEALINGS IN THE SOFTWARE.
+                
+                For more information, please refer to <http://unlicense.org/>`
+    } else {
+        return "This project is not licensed. "
+    }
+}
+
 inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "What is your full name? ",
+        validate: (value) => {
+            if (value) {
+                return true
+            } else {
+                return "Please enter your name to continue. "
+            }
+        }
+    },
     {
         type: "input",
         name: "title",
@@ -103,7 +212,7 @@ inquirer.prompt([
         type: "list",
         name: "license",
         message: "Which license would you like to use for your project? ",
-        choices: ["Apache License 2.0", "MIT License", "Creative Commons License", "GNU General Public License v2.0", "Mozilla Public License 2.0", "The Unilicense", "No License"],
+        choices: ["Apache License 2.0", "MIT License", "GNU General Public License v2.0", "Mozilla Public License 2.0", "The Unilicense", "No License"],
         validate: (value) => {
             if (value) {
                 return true
@@ -111,23 +220,6 @@ inquirer.prompt([
                 return "Please select a license to continue. "
             }
         },
-        filter: (input) => {
-            if (input == "Apache License 2.0") {
-                return input + " [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
-            } else if (input == "MIT License") {
-                return input + " [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
-            } else if (input == "Creative Commons License") {
-                return input + " [![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)"
-            } else if (input == "GNU General Public License v2.0") {
-                return input + " [![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)"
-            } else if (input == "Mozilla Public License") {
-                return input + " [![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
-            } else if (input == "The Unilicense") {
-                return input + " [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
-            } else {
-                return "You have chosen not to include a license. "
-            }
-        }
     },
     {
         type: "input",
